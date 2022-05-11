90be9968c531f1722e1ac4bfc9f5020a8ca6b658
Ismail <ismajovic20@gmail.com>
2022-05-11 23:25:42 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,62 @@
+const ime = 'max'
+//ime = 39; grsska
+
+let godine = 30 ;
+godine = 'rec';
+
+
+console.log(5 === 5);
+
+console.log(5 !== 5);
+
+console.log(5 == '5');
+
+console.log(5 === 5);
+//||   //or operator radi sdamo ako je jedan tacan i vrsti prvog tracnog
+
+// ! negacija 
+
+// && and operatoer radi samo ako su svi tacni i uzima vrednost 
+
+for( let e =0; e < 100; e++){
+    console.log(e);
+}
+
+/*while(uslov){
+    radi nesto
+}
+*/
+let suma = 0;
+
+for(let i=0; i<15; i++){
+ if(i % 2 !==0){
+     suma += i;
+ }
+}
+console.log(suma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*alert('prvi')
+console.log("javascript prvi ispias na konzoli");*/
