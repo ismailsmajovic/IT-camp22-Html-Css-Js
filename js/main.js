decb0c93c9bc17d7607e4ee7242c05cb38eb0fcf
Ismail <ismajovic20@gmail.com>
2022-05-11 23:29:55 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,33 +1,4 @@
-/*for(let i = 0; i < 100; i++){
-    if(i % 3 == 0 && i % 5 == 0){
-        console.log('FIZZBUZZ');
-    }else if(i % 3 == 0){
-        console.log('FIZZ');
-    }else if(i % 5 == 0){
-        console.log('BUZZ');
-    }else{
-        console.log(i);
-    }
-}*/
-for(let i =0; i < 100; i++){
-    switch(i){
-        case "1":
-            i % 3 == 0;
-            console.log('FIZZ');
-            break;
 
-        case "2" :
-            i % 5 == 0;
-            console.log('BUZZ');
-            break;
-        case "3" :
-            i % 3 == 0 && i % 5 == 0;
-            console.log('FIZZBUZZ');
-        case "4" :
-            console.log(i);
-    }
-        
-}
 
 
 
