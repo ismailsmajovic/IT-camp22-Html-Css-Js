c6bfd6c0abf47153904a4d2a169d3333cd24f911
Ismail <ismajovic20@gmail.com>
2022-05-21 19:31:03 +0200

    init

--- function/main.js
+++ function/main.js
@@ -22,8 +22,12 @@
   }
   mycall(5, 5,mydis);*/
 
+
+
+  //domcaci za vezbe
+  /*
 let niz = [23,89,0, 'string'];
-let niz2 = [32,98,23,45,"any",-0]
+let niz2 = [32,98,23,45,"any",5]
 
 const izvadiNiz =()=>{
     if (niz[2]) {
@@ -35,6 +39,32 @@ const izvadiNiz =()=>{
     }
 }
 izvadiNiz();
+*/
+
+//druga vezba
+const niz3 =[2412,93,0,-0,null, 3,[33,32,32],32];
+
+const takeNiz =() =>{
+    for (let index = 0; index <niz3.length; index++) {
+        //const nizz = array[index];
+        if (niz3[3]) {
+            console.log(niz3[3]);
+        }else if(niz3[4]){
+            console.log(niz3[4]);
+        }else if(niz3[4]){
+            console.log(typeof(niz3[4]));
+        }
+        else{
+            console.log(typeof(niz3[4])); //console.log('ovo je neodredjeno');
+        }
+    }
+}
+takeNiz();
+
+const isSunny = true;
+
+isSunny ?console.log('naocare'):console.log('kisobran');
+
 
 
 
