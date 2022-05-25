d6f38eb381b9f6041c165c8fad05de857a7c7916
Ismail <ismajovic20@gmail.com>
2022-05-25 16:54:16 +0200

    init

---
+++ function2/cas.js
@@ -0,0 +1,78 @@
+/*if (+num <=50) {
+        const num =  promt('unsite veci broj')
+    }else if(+num > 50){
+        const num = prompt('unsite manji broj')
+    }else{
+         const num = prompt('NE UNOSITE GLUPOSTI')
+    }*/
+//window.addEventListener('load',unesiBroj);
+
+
+
+
+/*
+const mnz =(a, b, c = false)=>{
+    if ( c) {
+        return (a+b+c);
+    }else{
+        return (a+ b);
+    }
+}
+console.log(mnz(2,4,5));
+
+window.addEventListener('load',unesiBroj);
+ //drugo nesto
+ */
+
+/*const unesiBroj =(a, b) =>{
+    let zbir = brojA + brojB;
+    return zbir;
+}
+
+console.log(unesiBroj(15,5));*/
+//const zbir = brojA + brojB;
+
+/*function saberi(a, b){
+    const zbir = a + b;
+    return (zbir)
+}  
+console.log(saberi(41,5));
+*/
+
+
+
+
+
+
+
+
+
+//nebitno
+/*let a =5; 
+let b = 4;
+ let c = a+ b;
+*/
+ /*const sab = (inch)=>{
+   const zbir = inch * 2.5;
+   return zbir;
+ }
+console.log(sab(40));
+*/
+//alert('age');
+
+
+/*
+const age = promt('Insert your age:');
+
+if(+age < 18 ){
+    console.log('punolotan si')
+}else if (+age > 18){
+    console.log('Dete si:')
+}else{
+    console.log('bedan si');
+}*/
+//alert('age')
+//alert('age');
+
+
+//const age = prompt('insert your age:')
