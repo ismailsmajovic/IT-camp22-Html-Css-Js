0f6265e46fc2352537b938e8526232fad076010b
Ismail <ismajovic20@gmail.com>
2022-05-26 14:53:21 +0200

    init

---
+++ functionArray/main.js
@@ -0,0 +1,85 @@
+/*const years = [23, 43, 87, 13, 894, 92, 17];
+
+const findTeen = () =>{
+    const prazan =[];
+
+    for (let age of ages) {
+        if(age > 18){
+             prazan.push(age);
+        }
+        
+    }return prazan;
+}
+console.log(findTeen(years));
+
+//second exam.
+*/
+/*const godine =[23,43,87, 13,894,17, 14, 15];
+
+function nadji(ages){
+    const prazaNiz =[];
+    for(let age of ages){
+        if (age > 18) {
+            prazaNiz.push(age)
+        }
+    }return prazaNiz;
+}
+console.log(nadji(godine));
+
+//three
+const godine2 = [23, 43, 87, 13, 894, 17, 14, 15];
+
+const nadji2 =(ages)=>{
+
+    const prazanNiz2 =[];
+
+    for (let age of ages) {
+        if(age < 18){
+            prazanNiz2.unshift(age)
+        }
+        
+    }return prazanNiz2;
+}
+console.log(nadji2(godine2));
+
+//four
+const godine3 =[23, 43, 87, 13, 894, 17, 14, 15];
+ const godine32 =[33, 55, 75,9];
+
+const nadji3 =(ages)=>{
+    const prazanNIz3=[];
+    for(let age of ages){
+        if(age > 18){
+            prazanNIz3.push(age);
+        }
+    }return prazanNIz3;
+}
+console.log(nadji3(godine32));
+
+//five
+const godine4=[23, 43, 87, 13, 894, 17, 14, 15];
+
+const nadji4=(ages) =>{
+    const prazanNiz4 =[2, 34];
+
+    for (let age of ages) {
+        if(age > 18){
+            prazanNiz4.push(age);
+        }
+        
+    }return prazanNiz4;
+}
+console.log(nadji4(godine4))
+*/
+
+
+
+
+
+
+
+
+//FUNKCIJE SA NIZOVIMA
+//sest
+const niz4 =[23, 43, 87, 13, 894, 17, 14, 15];
+niz4.unshift(niz4);
