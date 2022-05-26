c52322102b85e4354bda1fd93faad1c1a6ac2ab0
Ismail <ismajovic20@gmail.com>
2022-05-26 04:07:37 +0200

    init

---
+++ function/homework2.js
@@ -0,0 +1,29 @@
+const add =(a, b) =>{
+    const num1 =prompt('unesi prvi broj:');
+    const num2 = prompt('unesi drugi broj:');
+
+    const sum = +num1 + +num2;
+    prompt('Zbir vasa dva broja je:', +sum);
+    return (+num1 + +num2);
+}
+add();
+
+
+
+
+
+
+
+
+
+
+
+
+/*const add =(a,b) =>{
+    const broj1 = prompt('Unesi prvi broj:');
+    const broj2 = prompt('Unesi drugi broj:');
+
+    const sum = a + b;
+    return (a + b);
+}
+prompt(add(12, 1));*/
