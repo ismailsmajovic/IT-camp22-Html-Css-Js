d8459b3e9575158ba97c356243f9c58d0b016338
Ismail <ismajovic20@gmail.com>
2022-06-01 17:08:12 +0200

    init

--- map/map.js
+++ map/map.js
@@ -1,4 +1,4 @@
-const DATA1 =[1,3, 5, 2, 7, 6, ];
+/*const DATA1 =[1,3, 5, 2, 7, 6, ];
 
 //el > 2 = el 7
 //el > 2 && el < 8  =(el * 4 ) /2
@@ -13,3 +13,28 @@ const dogAgeHuman =(ages)=>{
     });return dogAgeHuman;
 };
 console.log(dogAgeHuman(DATA1));
+*/
+
+//again
+/*
+const data2 = [1,3, 5, 2, 7, 6,];
+
+//el > 2 = el 7
+//el > 2 &&  el < 8  =(el * 4 )/2
+
+
+const dogAgeHuman =(ages)=> {
+    const humanAge = ages.map((age) => {
+        if(age > 2 &&  age < 8){
+            return  (age * 4 )/2;
+        }else if(age > 2){
+            return age * 7;
+        }else{
+            return age;
+        }
+    });return humanAge;
+}
+console.log(dogAgeHuman(data2));
+*/
+
+//again
