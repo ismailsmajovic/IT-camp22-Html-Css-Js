74e5e3bff57f9cfef05541ed410dbadaadaa1665
Ismail <ismajovic20@gmail.com>
2022-06-08 17:57:14 +0200

    init

---
+++ objects/object.js
@@ -0,0 +1,26 @@
+/*const broj = 20;
+
+const broj2 = Math.SQRT2(broj);
+
+console.log(broj2);*/
+/*
+const vreme =()=>{
+    let date = new Date();
+    //let cas = now.getHours();
+    //let minut = now.GetMinutes();
+    //let secoond = now.GetSecond(
+
+    console.log(date);
+    //return date;
+}
+setInterval(vreme, 1000);
+*/
+const myCar = {
+    marka: 'audi',
+     mercedes:{
+        boja: 'crna'
+    },
+
+    boja: 'crvena'
+}
+console.log('moj auto je:', myCar.mercedes.boja)
