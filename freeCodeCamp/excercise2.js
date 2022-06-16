88fd61dec54f4d8ef28e17b362eaf6d676d30269
Ismail <ismajovic20@gmail.com>
2022-06-16 16:53:00 +0200

    init

---
+++ freeCodeCamp/excercise2.js
@@ -0,0 +1,10 @@
+function randomFraction() {
+
+    // Only change code below this line
+    //const num = Math.random((1) * 0).toFixed(2);
+  
+    return Math.random();
+  
+    // Only change code above this line
+  }
+  console.log(randomFraction())
