e463592fa70dcd9e02e15e2fc96f45e7481f8394
Ismail <ismajovic20@gmail.com>
2022-06-18 16:43:18 +0200

    bind

--- call-bint-apply/bind2.js
+++ call-bint-apply/bind2.js
@@ -12,10 +12,17 @@ function withBintFunction(colors, rating){
     return `${this.mark} ${this.model} ${colors} ${rating}`
 }
 
+
+// with CALL function
 console.log(withBintFunction.call(myComputers, 'ima crvenu(call) boju, i ocenu', 3));
 
+
+//with APPLY function
 console.log(withBintFunction.apply(myComputers, ['ima belu (apply) boju, i ocenu', 4]));
 
+
+
+//with BIND function
 const withBind = withBintFunction.bind(myComputers);
 
 console.log(withBind('ima crnu(bind) boju, i ocenu', 5))
