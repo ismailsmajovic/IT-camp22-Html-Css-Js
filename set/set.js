cc1489aa3ef113ae09922a5114fc7456533770e4
Ismail <ismajovic20@gmail.com>
2022-06-21 15:53:09 +0200

    init

--- set/set.js
+++ set/set.js
@@ -1,79 +1,10 @@
-/* person ={
-    name:'John',
-    age:45,
-    job:44,
+// OBjekat Set nam omgucava da skladistimo jedinstven tip podatka(primitivnosg ili referentnog), tj. mecmop imati duple vrednime
 
-}*/
+const ime = [22, 22, 22, 22, 22,];
 
-//values()
-//keys()
-//object.is()
-//seal()
-//enteries()
-//isArray()
-
-/*const names = ['nesrtn', 'inrif', 'rfsf', 'efews'];
-
-const newNames = [...names]
-
-console.log(newNames)*/
-
-const name = ['33', '33', '44', '44'];
-
-const nema = [...new Set(name)];
-
-console.log(nema)
+const ime2 = [...new Set(ime)];         //koristimo spread(...) oprator  sa SET da bi nam vratili vrednost u obliku niza
 
 
-const newMovies =()=>{
+console.log(ime)                        // Output: [ 22, 22, 22, 22, 22 ]
 
-}
-
-/*
-\\\
-arr = [2,3,12,65,22]
-target =34
-*/
-
-const newarr = [23, 45, 54, 23 ,43];
-
-const newarr2 = [... newarr, 'samou ubacuj']
-
-console.log(newarr2);
-
-
-/*function withBintFunction(colors, rating){
-    return `${this.mark} ${this.model} ${colors} ${rating}`;
-}
-
-
-// with CALL function
-console.log(withBintFunction.call(myComputers, 'ima crvenu(call) boju, i ocenu', 3));
-
-
-//with APPLY function
-console.log(withBintFunction.apply(myComputers, ['ima belu (apply) boju, i ocenu', 4]));
-
-
-
-//with BIND function
-const withBind = withBintFunction.bind(myComputers);
-
-console.log(withBind('ima crnu(bind) boju, i ocenu', 5))*/
-
-
-
-
-
-
-/*const name = ['33', '33', '44', '44'];
-
-const nema = [...new Set(name)];
-
-console.log(nema)
-
-
-const newMovies =()=>{
-
-}*/
-
+console.log(ime2)                    //Output: [22]  dakle kad primenitmo SET onda vracamo samo jednu jedinstvnu vrednost bez duplikata
